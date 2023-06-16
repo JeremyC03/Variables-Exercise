c74a50c271ecd8cc019e0484f4f49a1c162805a3
Jeremy Cabrera <j3r3myc03@yahoo.com>
2023-06-16 12:13:21 -0400

    Did Variables Exercise

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,29 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string myName;
+            myName = "Jeremy";
+
+            int myAge;
+            myAge = 28;
+
+            char myInitial;
+            myInitial = 'J';
+
+            bool statement;
+            statement = true;
+
+            double number;
+            number = 3;
+
+            decimal year;
+            year = 2023;
+
+            Console.WriteLine($"Hello, my name is {myName}, and I am {myAge}.");
+            Console.WriteLine($"My initial is {myInitial} and I like the number {number}.");
+            Console.WriteLine($"This statement is {statement}.");
+            Console.WriteLine($"The year is {year}.");
+
         }
     }
 }
